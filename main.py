4cc3e0795117ec703136f35c7c1d7b1751e541a4
Ildebrando Mora <ilder948@gmail.com>
2023-01-05 14:57:18 -0500

    request and response body

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
-from fastapi import FastAPI
+#Python
+from typing import Optional
+
+#Pydantic
+from pydantic import BaseModel
+
+#FastApi
+from fastapi import FastAPI, Body
+from fastapi import Body
+
 
 app = FastAPI()
+
+#Models
+
+class Person(BaseModel):
+    firstName: str
+    lastName: str
+    age: int
+    hairColor: Optional[str] = None
+    isMarried: Optional[bool] = False
+
 
 @app.get('/')
 def home():
     return {'Hello': 8989}
 
+#Request and Response Body
+
+@app.post('/person/new')
+def createPerson(person: Person = Body(...)):
+    return {'DATA': [person], 'COUNT': len([person])}
