f1e9bcecd132eb12daf6100037ce567855e6f568
Ildebrando Mora <ilder948@gmail.com>
2023-01-06 12:38:06 -0500

    authomatic query ang path examples

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 #Python
 from typing import Optional
+from enum import Enum
 
 #Pydantic
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 #FastApi
 from fastapi import FastAPI
@@ -13,17 +14,34 @@ app = FastAPI()
 
 #Models
 
+class HairColor(Enum):
+    white = 'white'
+    brow = 'brow'
+    black = 'black'
+    blonde = 'blonde'
+    red = 'red'
 
 class Location(BaseModel):
     city: str
     state: str
 
 class Person(BaseModel):
-    firstName: str
-    lastName: str
-    age: int
-    hairColor: Optional[str] = None
-    isMarried: Optional[bool] = False
+    firstName: str = Field(..., min_lenght=1, max_length=50, example="Carlos" )
+    lastName: str =  Field(..., min_lenght=1, max_length=50, example="Lopez")
+    age: int = Field(..., gt=0, le=115, example=89)
+    hairColor: Optional[HairColor] = Field(default= None, example="brow")
+    isMarried: Optional[bool] = Field(default=None, example=False)
+
+    # class Config:
+    #     schema_extra = {
+    #         "example": {
+    #             "firstName": "Ildebrando",
+    #             "lastName": "Mora Valdes",
+    #             "age": 34,
+    #             "hairColor": "black",
+    #             "isMarried": True,
+    #         }            
+    #     }
 
 
 @app.get('/')
@@ -46,12 +64,14 @@ def show_person(
         min_length=1, 
         max_length=50,
         title='Person Name',
-        description="this is the person name, It's between 1 and 500 characteres"
+        description="this is the person name, It's between 1 and 500 characteres",
+        example="Rocio"
         ),
     age: str = Query(
         ...,
         title='Person Age',
-        description="this is the person age, It's required"
+        description="this is the person age, It's required",
+        example=56
         )
     ):
     return {name: age}
@@ -59,13 +79,14 @@ def show_person(
 
 #validaciones path parameters
 
-@app.get('/person/detail/{person_id}/car/{car_id}')
+@app.get('/person/detail/{person_id}')
 def show_person(
     person_id: int = Path(
         ...,
         gt=0,
         name='Person Id',
-        description="this is the person id, It's required"
+        description="this is the person id, It's required",
+        example= 123
         )
 ):
     return {'personId': person_id}
@@ -82,8 +103,8 @@ def update_person(
         gt=0
     ),
     person: Person = Body(...),
-    location: Location = Body(...)
+    #location: Location = Body(...)
 ):
-    result = person.dict()
-    result.update(location.dict())
-    return {"id": person_id, "person": result}
+    #result = person.dict()
+    #result.update(location.dict())
+    return {person}
